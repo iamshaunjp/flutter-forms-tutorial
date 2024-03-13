ca2b78943d427eec8756740623ce3da9523a6661
Shaun <shaun@thenetninja.co.uk>
2024-03-13 11:15:28 +0000

    lesson-4

--- lib/home.dart
+++ lib/home.dart
@@ -11,8 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  final TextEditingController _emailController = TextEditingController();
-
+  final _formGlobalKey = GlobalKey<FormState>();
 
   final List<Todo> todos = [
     const Todo(
@@ -47,21 +46,35 @@ class _HomeState extends State<Home> {
             Expanded(child: TodoList(todos: todos)),
         
             // form stuff below here
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              // obscureText: true,
-              decoration: const InputDecoration(
-                label: Text('Email address'),
-              ),
-              controller: _emailController,
-            ),
-            
-            const SizedBox(height: 20),
-            FilledButton(
-              onPressed: () {
-                print(_emailController.text.trim());
-              },
-              child: const Text('print email'),
+            Form(
+              key: _formGlobalKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+
+                  // todo title
+
+
+                  // todo description
+
+
+                  // priority
+                 
+
+                  // submit button
+                  const SizedBox(height: 20),
+                  FilledButton(
+                    onPressed: () {},
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.grey[800],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    child: const Text('Add'),
+                  )
+                ],
+              ), 
             )
             
           ],
